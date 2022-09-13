bdad7e4c999c8ee0d883bdd06aaaf016cc0defc5
shahboz007 <olmasovshahboz007@gmail.com>
2022-09-13 11:19:34 -0700

    create icons component

---
+++ src/components/Icons.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../styles/Icon.module.css";
+
+function Icons() {
+  return (
+    <i className={styles.icon, styles.person}></i>
+  )
+}
+
+export default Icons
