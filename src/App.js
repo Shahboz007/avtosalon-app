17ddff490dc2edf5be754bcff23685ea0b7db81c
shahboz007 <olmasovshahboz007@gmail.com>
2022-09-13 10:40:58 -0700

    create pages with connected antd css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CarCategory from './components/CarCategory';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Review from './components/Review';
 
 const App = () => (
-  <>
-    salom
-  </>
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/category/' element={<CarCategory />} />
+      <Route path='/category/:category/:id' element={<Review />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
---
+++ src/components/CarCategory.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CarCategory() {
+  return (
+    <div>CarCategory</div>
+  )
+}
+
+export default CarCategory
